160a74eac28c187bdc0531a17372d92d57a0d8ea
bayusatmoko <bsatmoko@gmail.com>
2024-05-06 18:43:52 +0700

    Test CI CD

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div style={{ background: 'white', minHeight: '100vh' }}>
       <nav style={{ height: '5vh', backgroundColor: '#0e2127', display: 'flex', alignItems: 'center', justifyContent: 'flex-start', paddingLeft: '20px' }}>
-        <span style={{ color: 'white', fontSize: '1.2rem', fontWeight: 'bold' }}>GeoApp</span>
+        <span style={{ color: 'white', fontSize: '1.2rem', fontWeight: 'bold' }}>GeojsonApp</span>
       </nav>
       <main style={{ minHeight: '95vh' }}>
         <DynamicInteractiveGlobe />
